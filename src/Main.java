6eddabb8431427aa57b7d51f83ade2b4537720ca
NanaYaw805 <arhinyaw64@gmail.com>
2024-03-22 16:16:04 +0000

    Implementing the proxy pattern

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,18 @@
-import AdapterPattern.Avafilters.Caramel;
-import AdapterPattern.CaramelFilter;
-import AdapterPattern.Image;
-import AdapterPattern.ImageView;
-import AdapterPattern.VividFilter;
-import Bridge.RemoteControl;
-import Bridge.sony;
-import ChainOfResponsibility.*;
-import CompositePattern.Group;
-import CompositePattern.Shape;
-import Decorator.CloudStream;
-import Decorator.CompressedCloudStream;
-import Decorator.EncryptedCloudStream;
-import Decorator.Stream;
-import FacadePattern.Message;
-import FacadePattern.NotificationServer;
-import FacadePattern.NotificationService;
-import FlyweightPattern.Point;
-import FlyweightPattern.PointIconFactory;
-import FlyweightPattern.PointService;
-import ObserverPattern.Books;
-import ObserverPattern.DataSource;
-import ObserverPattern.Library;
-import Programming_to_an_interface.Animal;
-import Programming_to_an_interface.Cat;
-import TemplateMethodPattern.GenerateReportTax;
-import TemplateMethodPattern.TransferMoneyTask;
-import VisitorPattern.AnchorNode;
-import VisitorPattern.HeadingNode;
-import VisitorPattern.HighLightOperation;
-import VisitorPattern.HtmlDocument;
-
-import java.util.ArrayList;
-import java.util.List;
+import Proxy.Ebook;
+import Proxy.EbookProxy;
+import Proxy.Library;
+import Proxy.RealEbook;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            var remoteControl = new RemoteControl(new sony());
-            remoteControl.turnOff();
-
-
+       var library = new Library();
+       String[] fileNames = new String[]{"s","d","d","e"};
+       for (var fileName:fileNames){
+           library.add(new EbookProxy(fileName));
+           library.openEbook("s");
+       }
 
     }
 
